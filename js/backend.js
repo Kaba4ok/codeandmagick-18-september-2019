3414ddab1ac681867ce3b42125f4784e6662eca9
Сергей <s.p.prohorov@gmail.com>
2019-10-13 02:14:02 +0300

    Оптимизация модуля backend.js

--- js/backend.js
+++ js/backend.js
@@ -5,7 +5,9 @@
   var URL_GET = 'https://js.dump.academy/code-and-magick/data';
   var URL_POST = 'https://js.dump.academy/code-and-magick';
 
-  var load = function (onLoad, onError) {
+  var TIMEOUT = 10000;
+
+  var request = function (method, url, onLoad, onError, data) {
     var xhr = new XMLHttpRequest();
 
     xhr.responseType = 'json';
@@ -26,39 +28,19 @@
       onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
     });
 
-    xhr.timeout = 10000;
+    xhr.timeout = TIMEOUT;
 
-    xhr.open('GET', URL_GET);
+    xhr.open(method, url);
 
-    xhr.send();
+    xhr.send(data);
+  };
+
+  var load = function (onLoad, onError) {
+    request('GET', URL_GET, onLoad, onError);
   };
 
   var save = function (data, onLoad, onError) {
-    var xhr = new XMLHttpRequest();
-
-    xhr.responseType = 'json';
-
-    xhr.addEventListener('load', function () {
-      if (xhr.status === 200) {
-        onLoad();
-      } else {
-        onError('Статус ответа: ' + xhr.status);
-      }
-    });
-
-    xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения');
-    });
-
-    xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-    });
-
-    xhr.timeout = 10000;
-
-    xhr.open('POST', URL_POST);
-
-    xhr.send(data);
+    request('POST', URL_POST, onLoad, onError, data);
   };
 
   window.backend = {
